281994add54a9b606195d29ec4c0789aa8a1d1a8
Sadiqul Rahman Rupom <153832903+sadiqsixnine@users.noreply.github.com>
2024-05-11 16:03:42 +0600

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -29,10 +29,10 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/Qy7kYbBMsZ";
-      const instaLink = "https://www.instagram.com/sadiq.sixnine69";
-      const facebookLink = "https://www.facebook.com/LINK.COPY.KOROS.KEN.VOKTO";
-      const youtubeLink = "https://www.youtube.com/@sadiqsixnine";
+      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
+      const githubLink = "https://github.com/GlaceYT";
+      const replitLink = "https://replit.com/@GlaceYT";
+      const youtubeLink = "https://www.youtube.com/@GlaceYT";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
@@ -40,7 +40,7 @@ module.exports = {
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- Instagram - ${instaLink}\n- Facebook - ${facebookLink}\n- YouTube - ${youtubeLink}`)
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
             .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
             .setTimestamp();
 
